158073b0270a6a1576395ce9cc566a073173cef8
alahmad-loay <loay.alahmad1@gmail.com>
2023-11-28 08:55:21 +0200

    map and health addition

--- main.py
+++ main.py
@@ -5,30 +5,34 @@ import pygame
 pygame.init()
 
 # CREATE SCREEN/WINDOW
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((800, 700))
 pygame.display.set_caption("shooter game thingy idk")
 clock = pygame.time.Clock()
 
+# LOAD BACKGROUND
+background = pygame.transform.scale(pygame.image.load("images/MAP.png").convert(), (800, 700))
 
-# LOAD IMAGES
 
 # PLAYER
 class Player(pygame.sprite.Sprite):
     # INITIAL CONSTRUCTOR
     def __init__(self):
         super().__init__()
-        self.pos = pygame.math.Vector2(400, 500)
-        self.image = pygame.transform.rotozoom(pygame.image.load('images/playerCharacter.png').convert_alpha(), 0, 0.3)
-        # WHAT WE ROTATE FOR HIGHER QUALITY AND LESS BUGS
+        self.pos = pygame.math.Vector2(200, 500)
+        self.image = pygame.transform.rotozoom(pygame.image.load('images/playerCharacter.png').convert_alpha(), 0, 0.18)
+        # WHAT WE ROTATE FOR HIGHER QUALITY AND FEWER BUGS
         self.base_player_image = self.image
         # TO CHECK HITBOX
         self.hitbox_rect = self.base_player_image.get_rect(center=self.pos)
+        self.hitbox_rect.height = 30
         # TO DRAW PLAYER ON SCREEN
         self.rect = self.hitbox_rect.copy()
         # for bullet
         self.shoot = False
         self.shoot_cooldown = 0
-        self.gun_barrel_offset = pygame.math.Vector2(3, 2) # ill come back to it
+        self.health = 100
+        self.max_health = 100
+        self.gun_barrel_offset = pygame.math.Vector2(-10, -40)  # ill come back to it
 
     # ROTATION
     def player_rotation(self):
@@ -67,7 +71,6 @@ class Player(pygame.sprite.Sprite):
         else:
             self.shoot = False
 
-
     def is_shooting(self):
         if self.shoot_cooldown == 0:
             self.shoot_cooldown = 20
@@ -78,9 +81,54 @@ class Player(pygame.sprite.Sprite):
 
     # CHANGE POSITION
     def move(self):
-        self.pos += pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        new_pos = self.pos + pygame.math.Vector2(self.velocity_x, self.velocity_y)
+        if self.is_within_playable_area(new_pos):
+            self.pos = new_pos
         self.hitbox_rect.center = self.pos
         self.rect.center = self.hitbox_rect.center
+
+    def is_within_playable_area(self, new_pos):
+        # Define boundaries for the left square region
+        left_square = pygame.Rect(50, 110, 260, 430)  # Adjust these values as needed
+
+        # Define boundaries for the hallway region
+        hallway = pygame.Rect(310, 220, 180, 70)  # Adjust these values as needed
+
+        # Define boundaries for the right square region
+        right_square = pygame.Rect(490, 110, 260, 430)  # Adjust these values as needed
+
+        # Check if the new position is within the left square, hallway, or right square
+        if left_square.collidepoint(new_pos) or hallway.collidepoint(new_pos) or right_square.collidepoint(new_pos):
+            return True  # Player is within the left square, hallway, or right square
+        else:
+            return False  # Player is outside the left square, hallway, and right square
+
+    # DRAW HEALTH TEXT
+    def draw_health_text(self):
+        font = pygame.font.Font(None, 30)
+        health_text = font.render('Health:', True, (255, 255, 255))
+        text_rect = health_text.get_rect(center=((screen.get_width() // 2) - 100, 15))
+        screen.blit(health_text, text_rect)
+    # DRAW HEALTH BAR
+    def draw_health_bar(self):
+        # Calculate health ratio
+        health_ratio = self.health / self.max_health
+        # Set the dimensions of the health bar
+        bar_width = 100
+        bar_height = 10
+        # Calculate the width of the colored portion of the health bar
+        health_bar_width = int(bar_width * health_ratio)
+        # Create a surface for the health bar
+        health_bar_surface = pygame.Surface((bar_width, bar_height), pygame.SRCALPHA)
+        # Draw the background of the health bar
+        pygame.draw.rect(health_bar_surface, (255, 255, 255), (0, 0, bar_width, bar_height))
+        # Draw the colored portion representing the health
+        pygame.draw.rect(health_bar_surface, (255, 0, 0), (0, 0, health_bar_width, bar_height))
+        # Set the position to display the health bar at the top of the screen
+        health_bar_pos = (screen.get_width() // 2 - bar_width // 2, 10)
+        # Blit the health bar onto the screen
+        screen.blit(health_bar_surface, health_bar_pos)
+
 
     # UPDATE PLAYER STATE
     def update(self):
@@ -93,19 +141,20 @@ class Player(pygame.sprite.Sprite):
 
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, bullet_angle): # change x,y name
+    def __init__(self, x, y, bullet_angle):
         super().__init__()
-        self.image = pygame.transform.rotozoom(pygame.image.load('images/bullet.png').convert_alpha(), 0, 2)
-        self.rect = self.image.get_rect() #fix name later
+        self.image = pygame.transform.rotozoom(pygame.image.load('images/bullet.png').convert_alpha(), 0, 1.5)
+        self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         self.x = x
         self.y = y
         self.speed = 30
         self.bullet_angle = bullet_angle - 90
-        self.x_vel = math.cos(self.bullet_angle * (2*math.pi/360)) * self.speed # change name later
-        self.y_vel = math.sin(self.bullet_angle * (2*math.pi/360)) * self.speed #here too
+        self.x_vel = math.cos(self.bullet_angle * (2 * math.pi / 360)) * self.speed
+        self.y_vel = math.sin(self.bullet_angle * (2 * math.pi / 360)) * self.speed
         self.bullet_lifetime = 750
-        self.spawn_time = pygame.time.get_ticks() # time bullet was created
+        self.spawn_time = pygame.time.get_ticks()
+
     def bullet_movement(self):
         self.x += self.x_vel
         self.y += self.y_vel
@@ -113,43 +162,63 @@ class Bullet(pygame.sprite.Sprite):
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
         if pygame.time.get_ticks() - self.spawn_time > self.bullet_lifetime:
-            self.kill()  # if timer - the timer for the bullet creation is grater than bullet lifetime -  KILL IT
+            # if timer - the timer for the bullet creation is grater than bullet lifetime -  KILL IT
+            self.kill()
 
     def update_movement(self):
         self.bullet_movement()
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, position):
+        super().__init__()
+        self.image = pygame.image.load("images/yellow_ball.png").convert_alpha()
+        self.image = pygame.transform.rotozoom(self.image, 0, 0.05)
+        self.rect = self.image.get_rect()
+        self.rect.center = position
 
+
+all_sprites_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
 
 # CREATE INSTANCE OF PLAYER
 player = Player()
 
-all_sprites_group = pygame.sprite.Group()
-bullet_group = pygame.sprite.Group()
-
 all_sprites_group.add(player)
+
+enemy = Enemy((600, 400))
+enemy_group.add(enemy)
+all_sprites_group.add(enemy)
 
 # GAME LOOP
 running = True
 while running:
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill((245, 245, 220))
+    screen.blit(background, (0, 0))
 
     # HANDLE EVENTS/ pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # # RENDER PLAYER
-    # screen.blit(player.image, player.rect)
-    # # CALL FUNCTION TO UPDATE PLAYER STATE
-    # player.update()
+    player.draw_health_text()
+    player.draw_health_bar()
+
     pygame.draw.rect(screen, "red", player.hitbox_rect, width=2)
     pygame.draw.rect(screen, "yellow", player.rect, width=2)
 
     for bullet in bullet_group.sprites():
         bullet.update_movement()
 
+    # Check for collisions between the player's hitbox and the enemy's sprite
+    # CHANGE POSITION AND MAKE A FUNCTION DO NOT LEAVE IT IN GAME LOOP
+    for enemy in enemy_group.sprites():
+        if player.hitbox_rect.colliderect(enemy.rect):
+            player.health -= 30
+            enemy.kill()  # Remove the enemy if there's a collision
+
+    # RENDER PLAYER AND BULLET // CALL FUNCTION TO UPDATE THEIR STATES
     all_sprites_group.draw(screen)
     all_sprites_group.update()
 
